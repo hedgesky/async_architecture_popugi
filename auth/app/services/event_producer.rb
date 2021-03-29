bfdd460e7e6553b50df1b48b7be2fd23abcac775
Anton Chuchkalov <anton.chuchkalov@rakuten.com>
2021-03-29 22:45:34 +0200

    introduce RabbitQueue.new

--- auth/app/services/event_producer.rb
+++ auth/app/services/event_producer.rb
@@ -20,28 +20,13 @@ class EventProducer
   class << self
     private
 
-    def queue
-      @queue ||= connect_to_queue
-    end
-
     def to_attrs(account)
       account.attributes.slice('id', 'email')
     end
 
     def send_message(payload)
-      queue.publish(payload.to_json)
-    end
-
-    def connect_to_queue
-      conn = Bunny.new
-      conn.start
-
-      # haven't used RabbitMQ yet, and don't know all its features. But it's 11pm, so
-      # I don't have time to learn them. So for now, declare single queue, go against
-      # best practices and include both producer and consumer names to it.
-      #
-      # we'll leave handling connections, but that's fine for now
-      conn.create_channel.queue('auth-tasks')
+      # RabbitQueue.new('auth-tasks').push(payload)
+      RabbitQueue.new('auth-accounting').push(payload)
     end
   end
 end
